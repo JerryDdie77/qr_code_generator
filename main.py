5ee70ef2bf914e5b4fa23095baf613aa03735831
JerryDdie77 <shahterchannel@gmail.com>
2026-04-23 15:10:46 +0300

    some info

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def root():
       <input type="submit" value="QR-code"/>
    </form>'''
 
-# Триггерим Jenkins
+# Триггерим Jenkinss
 @app.route("/qr")
 def qr():
    msg = request.args.get('msg')
